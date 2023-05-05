e577d739a547db8413416810a92b0492c0e53243
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-05-05 16:04:53 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+config.py:
+TOKEN = "6247263850:AAG-jhUF0fujne4nJzNhgCTpOsE5SHP7Dag"
+
+#Список доступных валют
+keys = {
+    'рубль': 'RUB',
+	'евро': 'EUR',
+    'доллар': 'USD',
+	'юань': 'CNY',
+}
