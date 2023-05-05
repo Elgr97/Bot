76aa030348fbfe8b0e2dc6f255d68e3a531a5117
El_gr <112843132+Elgr97@users.noreply.github.com>
2023-05-05 21:47:20 +0700

    Update add.py

--- add.py
+++ add.py
@@ -7,12 +7,12 @@ bot = telebot.TeleBot(TOKEN)
 # Функция-обработчик сообщений
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
-    text = f"Привет, {message.chat.username}! Я - бот, который поможет тебе конвертировать валюты. Напиши мне сообщение в формате: <из какой валюты> <в какую валюту> <сумма для конвертации>. Например: доллар рубль 100\n Для того, чтобы увидеть список доступных валют нажмите /values"
+    text = f"Привет, {message.chat.username}! Я - бот, который поможет тебе конвертировать валюты. Напиши мне сообщение в формате: <из какой валюты> <в какую валюту> <сумма для конвертации>.\n Например: доллар рубль 100\n Для того, чтобы увидеть список доступных валют нажмите /values"
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['help'])
 def help(message: telebot.types.Message):
-    text = f"Привет, {message.chat.username}! Для начала работы с ботом нажми /start"
+    text = f"Привет, {message.chat.username}! Для начала работы с ботом нажми /start\n Для того, чтобы увидеть список доступных валют нажмите /values"
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
